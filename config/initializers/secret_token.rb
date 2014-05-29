53e85307c0e7d6831cb8e902df4774d71bfd4ca2
Fernando Daniel Barroso <fdbarroso27@gmail.com>
2014-05-29 13:45:16 -0300

    ScreenCast Proyect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task::Application.config.secret_token = '72d8c7dfc144f38140bfe6a386cb33d49ed59fa153547d401d9fca6502b7fc291da0ec154d8880171b527be322cda27e49696aa19d4a6fa64884fa6f3f348a6c'
